902856f886ceae08c5fae5ac1bdd078477c850ec
Autumn Jolitz <autumn.jolitz@gmail.com>
2024-10-07 16:27:22 -0700

    fix: deoptimize the site-packages

--- remove-py-if-pyc-exists.sh
+++ remove-py-if-pyc-exists.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 if [ -f "${1}c" ]; then
-    if ! case "$1" in *site-packages/pip/__pip-*) false ;; *) true ;; esac ; then
+    if ! case "$1" in *site-packages/*) false ;; *) true ;; esac ; then
         echo 'Skipping optimization of '"$1"
         exit 0
     fi
