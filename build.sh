9015f0d1e203417b6ad7d871329e45c3fe629891
Autumn Jolitz <autumn.jolitz@gmail.com>
2025-12-05 16:15:47 -0800

    feat: split out docker files to reduce privileged docker runs

--- build.sh
+++ build.sh
@@ -24,8 +24,21 @@ if ! >"$log" 2>&1 docker build \
     --build-arg "BASE_IMAGE_DIGEST=${BASE_IMAGE_DIGEST}" \
     --build-arg "PYTHON_VERSION=${PYTHON_VERSION}" \
     --build-arg "BUILD_ROOT=/d" \
-    -f Dockerfile.alpine \
-    -t "$IMAGE_TAG" \
+    -f buildroot/Dockerfile.alpine \
+    -t "${IMAGE_TAG}-buildroot" \
+    . ; then
+    >&2 echo 'Unable to build '"${IMAGE_TAG}-buildroot"'!'
+    >&2 cat "$log"
+    exit 8;
+fi
+if ! >"$log" 2>&1 docker build \
+    --build-arg "ALPINE_VERSION=${ALPINE_VERSION}" \
+    --build-arg "BASE_IMAGE_DIGEST=${BASE_IMAGE_DIGEST}" \
+    --build-arg "PYTHON_VERSION=${PYTHON_VERSION}" \
+    --build-arg "BUILD_ROOT=/d" \
+    --build-arg "SOURCE_IMAGE=${IMAGE_TAG}-buildroot" \
+    -f buildroot/Dockerfile.alpine \
+    -t "${IMAGE_TAG}" \
     . ; then
     >&2 echo 'Unable to build '"$IMAGE_TAG"'!'
     >&2 cat "$log"
